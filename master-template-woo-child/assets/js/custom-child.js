b3e46162d2ec56abe46e81cb8b6eb6ac17c49dfa
Angel Burgos <angelpublicista.1@gmail.com>
2020-12-15 15:23:57 -0500

    ajuste carrusel

--- master-template-woo-child/assets/js/custom-child.js
+++ master-template-woo-child/assets/js/custom-child.js
@@ -12,6 +12,33 @@ jQuery(function ($) {
         nextArrow: '<button type="button" class="slick-custom-arrow slick-custom-next shadow"><i class="fas fa-chevron-right"></i></button>',
         prevArrow: '<button type="button" class="slick-custom-arrow slick-custom-prev shadow"><i class="fas fa-chevron-left"></i></button>',
         autoplay: true,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 1440,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
 });
