45a53d5eb60ed489aa2d8302a50425befadfbfea
Angel Burgos <angelpublicista.1@gmail.com>
2020-12-16 18:04:16 -0500

    Avance single tours

--- master-template-woo-child/single-tours.php
+++ master-template-woo-child/single-tours.php
@@ -33,21 +33,49 @@ get_header();
                         </div>
                     </div>
 
-                    <h3 class="rgc-content-package__title text-center">Cartagena, Bol</h3>
+                    <h3 class="rgc-content-package__title text-center"><?php the_title(); ?></h3>
                     <div class="rgc-content-package__days mb-3 text-center">
-                        <span class="rgc-content-package__days-night"><i class="far fa-moon"></i> 4 noches</span>
-                        <span class="rgc-content-package__days-day"><i class="far fa-sun"></i> 3 días</span>
+                        <?php if(get_field('noches')): ?>
+                            <span class="rgc-content-package__days-night"><i class="far fa-moon"></i> <?php the_field('noches'); ?></span>
+                        <?php endif; ?>
+                        <?php if(get_field('dias')): ?> 
+                            <span class="rgc-content-package__days-day"><i class="far fa-sun"></i> <?php the_field('dias'); ?></span>
+                        <?php endif; ?>
                     </div>
 
-                    <p class="rgc-content-package__desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error doloremque ad non nemo ipsam eaque natus accusamus minima, ut, explicabo assumenda adipisci? Nostrum vero tempora quasi harum! Ipsum, quam eos.</p>
+                    <p class="rgc-content-package__desc"><?php the_content(); ?></p>
                 
                     <div class="rgc-content-package__price">
-                        <span class="rgc-content-package__price-reduced">$530.000 <br><span class="rgc-content-package__price-reduced__condition">por persona</span></span>
-                        <span class="rgc-content-package__price-normal">$530.000 <br><span class="rgc-content-package__price-normal__condition">por persona</span></span>
+                    <?php 
+                        // Info afiliados
+                        $afiliados = get_field('afiliados');
+                        $precio_afiliados = $afiliados['precio_afiliados'];
+                        $condicion_afiliados = $afiliados['condicion_precio'];
+
+                        // Info no afiliados
+                       $no_afiliados = get_field('no_afiliados');
+                       $precio_no_afiliados = $no_afiliados['precio_no_afiliados'];
+                       $condicion_no_afiliados = $no_afiliados['condiciones_no_afiliado'];
+                    ?> 
+                            <span class="rgc-content-package__price-reduced"> <?php echo $precio_afiliados; ?> <br><span class="rgc-content-package__price-reduced__condition"> <?php echo $condicion_afiliados; ?></span></span>
+                        <span class="rgc-content-package__price-normal"> <?php echo $precio_no_afiliados; ?> <br><span class="rgc-content-package__price-normal__condition"> <?php echo   $condicion_no_afiliados; ?></span></span>
                     </div>
 
+                    <?php
+                        $tipo_reserva = get_field('boton_reserva');
+
+                        if($tipo_reserva == "Whatsapp"){
+                            $link_reserva = api_whatsapp();
+                        } else {
+                            if(get_field('link_de_reserva')){
+                                $link_reserva = get_field('link_de_reserva');
+                            } else {
+                                $link_reserva = "#";
+                            }
+                        }
+                    ?>
                     <div class="text-center">
-                        <a href="#" class="button-master principal-button button-rounded my-3">RESERVAR AHORA</a>
+                        <a href="<?php echo $link_reserva; ?>" class="button-master principal-button button-rounded my-3" target="_blank">RESERVAR AHORA</a>
                     </div>
                 </div>
             </div>
@@ -55,6 +83,8 @@ get_header();
     </section>
 
     <!-- Incluye -->
+
+    
     <section class="padding-section rgc-include">
         <div class="container">
             <div class="rgc-box-title text-center mb-5">
@@ -63,30 +93,41 @@ get_header();
             </div>
             
             <div class="row rgc-package-features text-center">
+
+                <?php
+                $features = get_field('incluye');
+                foreach($features as $item_feature): ?>
+
+                <?php 
+                    switch($item_feature){
+                        case "Alojamiento":
+                            $icon = '<i class="fas fa-bed"></i>';
+                            break;
+                        case "Parqueadero":
+                            $icon = '<i class="fas fa-parking"></i>';
+                            break;
+                        case "Transporte":
+                            $icon = '<i class="fas fa-bus"></i>';
+                            break;
+                        case "Alimentación":
+                            $icon = '<i class="fas fa-utensils"></i>';
+                            break;
+                        case "Acceso a piscina":
+                            $icon = '<i class="fas fa-swimming-pool"></i>';
+                            break;
+                        case "Bebidas":
+                            $icon = '<i class="fas fa-cocktail"></i>';
+                            break;
+                        default:
+                            $icon = '<i class="fas fa-check-circle"></i>';
+                    }
+                ?>
+
                 <div class="col-6 col-md-4 rgc-package-features__box mb-3">
-                    <i class="fas fa-bed"></i>
-                    <span class="rgc-package-features__box-text">Feature 1</span>
+                    <?php echo $icon; ?>
+                    <span class="rgc-package-features__box-text"><?php echo $item_feature ?></span>
                 </div>
-                <div class="col-6 col-md-4 rgc-package-features__box mb-3">
-                    <i class="fas fa-bed"></i>
-                    <span class="rgc-package-features__box-text">Feature 1</span>
-                </div>
-                <div class="col-6 col-md-4 rgc-package-features__box mb-3">
-                    <i class="fas fa-bed"></i>
-                    <span class="rgc-package-features__box-text">Feature 1</span>
-                </div>
-                <div class="col-6 col-md-4 rgc-package-features__box mb-3">
-                    <i class="fas fa-bed"></i>
-                    <span class="rgc-package-features__box-text">Feature 1</span>
-                </div>
-                <div class="col-6 col-md-4 rgc-package-features__box mb-3">
-                    <i class="fas fa-bed"></i>
-                    <span class="rgc-package-features__box-text">Feature 1</span>
-                </div>
-                <div class="col-6 col-md-4 rgc-package-features__box mb-3">
-                    <i class="fas fa-bed"></i>
-                    <span class="rgc-package-features__box-text">Feature 1</span>
-                </div>
+                <?php endforeach; ?>
             </div>
         </div>
     </section>
